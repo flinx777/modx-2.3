290bf4550e25a61426f80117050a40adf430dbb8
flinx777 <flinx777@gmail.com>
2014-10-07 13:50:54 -0400

    adding versionx

---
+++ core/packages/googlesitemap-1.3.1-pl/preserved.php
@@ -0,0 +1,9 @@
+<?php return array (
+  '06674facf1b5a4c190b3a8b3be2a0c4f' => 
+  array (
+    'files' => 
+    array (
+      0 => '/home/my619des/public_html/modx-2.3/core/components',
+    ),
+  ),
+);
